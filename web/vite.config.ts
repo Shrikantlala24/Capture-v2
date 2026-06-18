16f55a834f1c8d7886cda76927b1ece601f741fe
Shrikantlala24 <shrikantlala2023@gmail.com>
2026-06-19 00:45:42 +0530

    web updates

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react, { reactCompilerPreset } from '@vitejs/plugin-react'
 import babel from '@rolldown/plugin-babel'
+import path from 'node:path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
   plugins: [
     tailwindcss(),
     react(),
